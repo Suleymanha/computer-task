3a3bda65270b263fc640b1d74e6509c175aa391d
Suleymanha <suleymanha@code.edu.az>
2021-09-30 11:30:42 +0400

    first commit

---
+++ Apple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace computer_task
+{
+    class Apple:Computer
+    {
+        public Apple(string name, string storage, int ram, int hd) : base(name, storage, ram, hd) 
+        {
+
+        }
+
+        public Apple(string name, string storage, int ram, int hd, int screen) : base(name, storage, ram, hd,screen)
+        {
+
+        }
+
+}
